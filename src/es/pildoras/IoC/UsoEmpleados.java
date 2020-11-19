a8862fb3ad264f07ee2f40bcd94e40ee289d97d8
aflasalas <alejandrofernandezlasalas20@gmail.com>
2020-11-19 16:08:43 -0300

    Inyeccion de dependencias IV. Video 12. Uso de archivo externo

--- src/es/pildoras/IoC/UsoEmpleados.java
+++ src/es/pildoras/IoC/UsoEmpleados.java
@@ -8,30 +8,19 @@ public class UsoEmpleados {
 		
 		///Pasos a seguir para usar el contenedor Spring
 		//1)Crear un contexto
-		
 		ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext("applicationContext.xml");
 	
-//		//2)Pedirle al archivo xml el Bean, el objeto
-//		Empleados juan = contexto.getBean("miEmpleado",Empleados.class);
-//		
-//		//3)Utilizar el Bean
-//		System.out.println(juan.getTareas());
-//		
-//		System.out.println(juan.getInforme());
-//		
-
-		
 		//2)Pedirle al archivo xml el Bean, el objeto
-		SecretarioEmpleado maite = contexto.getBean("miSecretarioEmpleado",SecretarioEmpleado.class);
+		DirectorEmpleado juan = contexto.getBean("miEmpleado",DirectorEmpleado.class);
 		
-		//3)Usar el Bean
-		System.out.println(maite.getTareas());
+		//3)Utilizar el Bean
+		System.out.println(juan.getTareas());
 		
-		System.out.println(maite.getInforme());
+		System.out.println(juan.getInforme());
 		
-		//Inyectamos campos con Spring
-		System.out.println("Email: "+ maite.getEmail());
-		System.out.println(maite.getNombreEmpresa());
+		System.out.println(juan.getEmail());
+		
+		System.out.println(juan.getNombreEmpresa());
 		
 		//4)Cerrar el xml
 		contexto.close();
